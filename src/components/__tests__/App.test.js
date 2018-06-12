0777b670325e9bc79b0dee88eb44556a550185d6
Michael <michael@topmiller@gmail.com>
2018-06-12 12:13:52 -0700

    added Enzyme Setup

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
+
+it('shows a comment box', ()=>{
+	const div = document.createElement('div')
+
+	ReactDOM.render(<App />, div)
+	expect(div.innerHTML).toContain('Comment Box')
+
+	ReactDOM.unmountComponentAtNode(div)
+})
