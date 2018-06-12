cc8a75ce4d5c5afe24d4ca2debdd29f4ee0e250f
Michael <michael@topmiller@gmail.com>
2018-06-12 13:06:23 -0700

    added textarea tests

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
+import { shallow } from 'enzyme';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 
-it('shows a comment box', ()=>{
-	const div = document.createElement('div')
+let wrapped;
 
-	ReactDOM.render(<App />, div)
-	expect(div.innerHTML).toContain('Comment Box')
+beforeEach(() => {
+	wrapped = shallow(<App />);
+})
 
-	ReactDOM.unmountComponentAtNode(div)
+it('shows a comment box', () => {
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it('shows a comment list', () => {
+	expect(wrapped.find(CommentList).length).toEqual(1)
 })
